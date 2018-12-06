3f718cd6be296bae6654d0a5540e810b05bf9848
aronschatz <aronschatz@aselabs.com>
2018-12-05 21:45:43 -0500

    Merge 1.3 wallet binaries.

--- wiremasternodescript.sh
+++ wiremasternodescript.sh
@@ -7,10 +7,26 @@
 # This script creates swap space, downloads all required packages, creates a masternode user, downloads the 
 # WIRE source and compiles. It then moves the binary to a system wide folder and installs a systemd script to have it autostart on boot. Aliases are created to always use the same conf directory. Use wire-cli to interact with the client.
 
+# This script creates swap space, downloads all required packages, creates a masternode user, and downloads the 
+# WIRE binaries. It then moves the binary to a system wide folder and installs a systemd script to have it autostart on boot. Aliases are created to always use the same conf directory. Use wire-cli to interact with the client.
+
+clear
+
+#Colors
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+
+UVER=18;
+
 clear
 cd ~
 echo "**********************************************************************"
-echo "* Ubuntu 16.04 is the required opearting system for this install.    *"
+echo "* Ubuntu 18.04 or 16.04 (x64) is the required opearting system for   *"
+echo "* this install.                                                      *"
 echo "*                                                                    *"
 echo "* WIRE script based masternode installation.                         *"
 echo "*                                                                    *"
@@ -18,19 +34,48 @@ echo "* Need help? Join the WIRE Discord: https://discord.gg/2482aX        *"
 echo "*                                                                    *"
 echo "* Root or sudo access is required for installation!                  *"
 echo "*                                                                    *"
-echo "*                                             Created by Aron Schatz *"
+echo "* S v1.1  W v1.3                              Created by Aron Schatz *"
 echo "**********************************************************************"
 echo && echo && echo
-echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-echo "!                                                     !"
-echo "! Please read each step before blindly hitting enter! !"
-echo "!                                                     !"
-echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+echo "Setting up initial environment..."
+
+sudo apt-get -y -qq update
+sudo apt-get -y -qq install dnsutils
+
+clear
+
+echo "**********************************************************************"
+echo "* Ubuntu 18.04 or 16.04 (x64) is the required opearting system for   *"
+echo "* this install.                                                      *"
+echo "*                                                                    *"
+echo "* WIRE script based masternode installation.                         *"
+echo "*                                                                    *"
+echo "* Need help? Join the WIRE Discord: https://discord.gg/2482aX        *"
+echo "*                                                                    *"
+echo "* Root or sudo access is required for installation!                  *"
+echo "*                                                                    *"
+echo "* S v1.1  W v1.3                              Created by Aron Schatz *"
+echo "**********************************************************************"
 echo && echo && echo
+
+#Check LSB release
+if [[ !($(lsb_release -d) == *16.04* || $(lsb_release -d) == *18.04*) ]]; then
+    echo -e "${RED}This operating system is not Ubuntu 16.04 or 18.04. Aborting.${NC}"
+    exit 1
+fi
+
+if [[ ($(lsb_release -d) == *16.04*) ]]; then
+    UVER=16;
+    echo "Detected Ubuntu 16.04..."    
+else
+    echo "Detected Ubuntu 18.04..."
+fi
+echo
 sleep 1
 
 # Check for systemd
-systemctl --version >/dev/null 2>&1 || { echo "systemd is required. Are you using Ubuntu 16.04?"  >&2; exit 1; }
+systemctl --version >/dev/null 2>&1 || { echo "systemd is required. Are you using Ubuntu 18.04 or 16.04?"  >&2; exit 1; }
 
 # Gather input from user
 read -e -p "Enter Masternode Private Key (e.g. 87PUDuUHk114BW46LLtCn2wWUKyVSCt23rmEQsqTYvJqQjTZtaz) : " key
@@ -38,7 +83,21 @@ if [[ "$key" == "" ]]; then
     echo "WARNING: No private key entered, exiting!!!"
     echo && exit
 fi
-read -e -p "What is the this server's IP Address? : " ip
+
+
+ip="$(dig @resolver1.opendns.com ANY myip.opendns.com +short)"
+regex='^([0-9a-fA-F]{0,4}:){1,7}[0-9a-fA-F]{0,4}$'
+
+if [[ $ip =~ $regex ]]; then
+    ip="[$ip]"
+fi
+read -e -p "The script detected this server IP as '${ip}'. Press enter if this is correct or manually enter the correct IP : " ipcheck
+if [[ ! -z $ipcheck ]]; then
+    ip=$ipcheck
+fi
+
+
+
 echo && echo "Pressing ENTER will use the default value for the next prompts."
 echo && sleep 1
 read -e -p "Add swap space? (Recommended) [Y/n] : " add_swap
@@ -74,12 +133,6 @@ fi
 sudo groupadd masternode
 sudo useradd -m -g masternode masternode
 
-# Update system 
-echo && echo "Upgrading system..."
-sleep 1
-sudo apt-get -y update
-sudo apt-get -y upgrade
-
 # Add Berkely PPA
 echo && echo "Installing bitcoin PPA..."
 sleep 1
@@ -90,8 +143,13 @@ sudo apt-get -y update
 # Install required packages
 echo && echo "Installing base packages..."
 sleep 1
-sudo apt-get -y install git libevent-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libdb4.8-dev libdb4.8++-dev libminiupnpc-dev virtualenv python-pip dh-autoreconf pkg-config build-essential libssl-dev libzmq3-dev libgmp3-dev libminiupnpc-dev ufw
+sudo DEBIAN_FRONTEND=noninteractive apt-get -o DPkg::Options::=--force-confdef -y install git libevent-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev libdb4.8-dev libdb4.8++-dev libminiupnpc-dev virtualenv python-pip dh-autoreconf pkg-config build-essential libssl-dev libzmq3-dev libgmp3-dev libminiupnpc-dev ufw unzip
 sleep 1
+
+# Update system 
+echo && echo "Upgrading system..."
+sleep 1
+sudo DEBIAN_FRONTEND=noninteractive apt-get -o DPkg::Options::=--force-confdef -y upgrade
 
 # Configure Firewall
 
@@ -106,17 +164,19 @@ echo && echo "Firewall installed and enabled!"
 
 
 # Download WIRE
-echo && echo "Downloading latest WIRE source and compiling..."
+echo && echo "Downloading v1.3 WIRE binary and installing"
 sleep 1
-git clone https://github.com/Social-Wallet-Inc/wire-core.git
-cd wire-core
-./autogen.sh
-./configure
-make
-cd src
-strip ./wire-cli
-strip ./wired
-strip ./wire-tx
+if [[ ("$UVER" == "16") ]]; then
+    wget https://github.com/AirWireOfficial/wire-core/files/2446021/wire-linux-16.04-1.3.0.0.zip
+    unzip wire-linux-16.04-1.3.0.0.zip
+else
+    wget https://github.com/AirWireOfficial/wire-core/releases/download/1.3.0/wire-ubuntu-18.04.v1.3.tar.gz
+    tar -xzvf ./wire-ubuntu-18.04.v1.3.tar.gz
+fi
+
+chmod +x ./wire-cli
+chmod +x ./wired
+chmod +x ./wire-tx
 
 # Install WIRE
 echo && echo "Installing WIRE..."
@@ -126,7 +186,7 @@ sudo mv ./wired /usr/local/bin
 sudo mv ./wire-tx /usr/local/bin
 
 # Create config for WIRE
-echo && echo "Configuring WIRE v1.0..."
+echo && echo "Configuring WIRE v1.3..."
 sleep 1
 rpcuser=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
 rpcpassword=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
@@ -146,31 +206,18 @@ masternodeprivkey='$key'
 masternode=1
 
 #addnodes
-addnode=108.61.95.114
-addnode=45.77.193.238
-addnode=45.32.133.67
-addnode=108.160.134.29
-addnode=207.148.86.107
-addnode=45.63.114.212
-addnode=45.32.22.184
-addnode=108.61.23.114
-addnode=104.156.254.203
-addnode=63.211.111.86
-addnode=144.202.78.8
-addnode=104.156.225.63
-addnode=173.199.71.62
-addnode=108.160.138.246
-addnode=45.77.0.247
-addnode=45.76.152.225
-addnode=144.202.8.219
-addnode=149.28.168.14
-addnode=108.61.224.93
-addnode=45.77.189.225
-addnode=45.76.232.61
-addnode=45.77.56.227
-addnode=149.28.37.186
-addnode=217.69.7.75
-addnode=202.182.107.213
+addnode=51.158.72.177
+addnode=51.15.224.33
+addnode=51.15.254.91
+addnode=159.69.188.123
+addnode=95.216.166.46
+addnode=95.216.191.165
+addnode=95.216.188.154
+addnode=x73pexnfmsnbhmea.onion:6520
+addnode=fiyylquk6ujdl6io.onion:6520
+addnode=5bozrozfrdmn6fws.onion:6520
+addnode=ukbd5bedakhsc4wj.onion:6520
+addnode=cssqmp3pl4ujm3zd.onion:6520
 ' | sudo -E tee /home/masternode/.wire/wire.conf
 sudo chown -R masternode:masternode /home/masternode/.wire
 
@@ -211,3 +258,7 @@ echo && echo "Wait until the blockchain syncs before starting your masternode fr
 
 
 source ~/.bash_aliases
+sleep 1
+
+echo && echo "Setup has completed."
+
